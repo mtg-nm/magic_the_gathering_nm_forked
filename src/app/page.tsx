c01db7019486a82b5376df514dbe21dd114b4812
Manilpit <matmeinilsen@gmail.com>
2026-02-21 20:00:31 +0100

    changed footer in main_to test deployment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -343,7 +343,6 @@ export default async function Home() {
             <div className="footer-content">
               <div className="footer-brand">NM Magic 2026</div>
               <div>Pilestredet 52 - Studenthuset, OsloMet • 7-9 august 2026</div>
-              <div>Made by Mathias Nilsen</div>
             </div>
           </div>
         </footer>
